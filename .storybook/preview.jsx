d9e1347f09c6103c322aca1c0e5086606218ed7b
Sridharan <sridharan@crayond.co>
2023-03-06 18:05:00 +0530

    build: 🏗  Storybook config

---
+++ .storybook/preview.jsx
@@ -0,0 +1,37 @@
+import React, { useMemo } from "react";
+// import { withRouter } from 'storybook-addon-react-router-v6';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import lightTheme from "../src/theme/lightTheme";
+import darkTheme from "../src/theme/darkTheme";
+// import '@fontsource/poppins/400.css';
+// import '@fontsource/poppins/500.css';
+// import '@fontsource/poppins/600.css';
+// import '@fontsource/poppins/700.css';
+
+export const decorators = [
+  (Story) => {
+    const muiTheme = useMemo(() =>
+      createTheme({
+        ...lightTheme
+      })
+    );
+
+    return (
+      <ThemeProvider theme={muiTheme}>
+        <Story />
+      </ThemeProvider>
+    );
+  },
+  // withRouter,
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    expanded: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
