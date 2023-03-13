3016460aac273f2b9e1174ab4776d9aeeaf1fe93
sridhar <sridharan@crayond.co>
2023-03-13 09:43:06 +0530

    refactor: ♻ search component props & story

--- src/components/search/Search.style.jsx
+++ src/components/search/Search.style.jsx
@@ -1,24 +1,22 @@
-import { styled } from '@mui/material';
-import TextField from '@mui/material/TextField';
+import { styled } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 export const StyledSearchBox = styled(TextField)(({ theme, disabled }) => ({
-    "& .MuiInputBase-formControl": {
-        padding: "10px 0px 10px 12px",
-        backgroundColor: disabled ? "#F5F5F5" : "",
-        height: "40px",
-        "& .MuiOutlinedInput-input": {
-            padding: "10px 12px 10px 0px",
-        }
+  "& .MuiInputBase-formControl": {
+    padding: "10px 0px 10px 12px",
+    backgroundColor: disabled ? "#F5F5F5" : "",
+    height: "40px",
+    "& .MuiOutlinedInput-input": {
+      padding: "10px 12px 10px 0px",
     },
+  },
 
-    "& .MuiFormHelperText-root": {
-        marginLeft: "0px"
-    },
+  "& .MuiFormHelperText-root": {
+    marginLeft: "0px",
+  },
 
-    "& .MuiSvgIcon-fontSizeMedium": {
-        paddingRight: "12px",
-        cursor: "pointer"
-    },
-
-
-}))
+  "& .MuiSvgIcon-fontSizeMedium": {
+    paddingRight: "12px",
+    cursor: "pointer",
+  },
+}));
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,47 +1,48 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { InputAdornment } from '@mui/material';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import ClearIcon from '@mui/icons-material/Clear';
-import { StyledSearchBox } from './Search.style';
-
+import ClearIcon from "@mui/icons-material/Clear";
+import { StyledSearchBox } from "./Search.style";
 
 const SearchField = ({ value, onClear, ...restProps }) => {
-
-    return (
-        <StyledSearchBox {...restProps}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment>
-                        <SearchIcon />
-                    </InputAdornment>
-                ),
-                endAdornment: (
-                    <InputAdornment onClick={value?.length > 0 ? onClear : () => { }}>
-                        {value?.length > 0 ? <ClearIcon /> : ""}
-                    </InputAdornment>
-                )
-            }} />
-    );
+  return (
+    <StyledSearchBox
+      {...restProps}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment>
+            <SearchIcon />
+          </InputAdornment>
+        ),
+        endAdornment: (
+          <InputAdornment onClick={value?.length > 0 ? onClear : () => {}}>
+            {value?.length > 0 ? <ClearIcon /> : ""}
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
 };
 
-
 SearchField.propTypes = {
-    value: PropTypes.string,
-    onChange: PropTypes.func,
-    disabled: PropTypes.bool,
-    placeholder: PropTypes.string,
-    fullWidth: PropTypes.bool,
+  variant: PropTypes.oneOf(["outlined"]),
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onClear: PropTypes.func,
+  disabled: PropTypes.bool,
+  fullWidth: PropTypes.bool,
 };
 
 SearchField.defaultProps = {
-    variant: 'outlined',
-    disabled: false,
-    placeholder: "",
-    fullWidth: false,
-    onClear: () => { },
+  variant: "outlined",
+  placeholder: "Search by",
+  value: "",
+  onChange: () => {},
+  onClear: () => {},
+  disabled: false,
+  fullWidth: false,
 };
-
-
 
 export default SearchField;
