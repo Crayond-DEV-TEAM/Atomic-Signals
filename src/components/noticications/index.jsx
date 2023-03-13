0bed8ca385aa84c5da8c4f710f12289b5a594a8f
rolexsuiya <surya.bca00@gmail.com>
2023-03-13 19:02:28 +0530

    notification component in progress

--- src/components/noticications/index.jsx
+++ src/components/noticications/index.jsx
@@ -1,65 +1,38 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { DeleteIcon } from '../../assets/deleteIcon';
 import { NotificationIconSvg } from '../../assets/notificationIocn';
-
-import { notification_card_style } from './style';
+import { CardStyle, ContentStyle, NotificationImg, StyleBox } from './style';
 
 export function NotificationCard(props) {
     const {
         className = '',
-        rootStyle = {},
         prescription = '',
         notificationTime = '',
-        url = '',
-        redirectFnc,
-        data = {},
-        is_read = false,
         ...rest
     } = props;
 
     return (
-        <Box
-            sx={{
-                ...notification_card_style.rootSx,
-                ...rootStyle,
-                backgroundColor: '#FFFFFF',
-                boxShadow: is_read === false ? '' : '0px 1px #dfe2e3',
-            }}
-            className={`${className}`}
-            {...rest}
-        >
-            <Box sx={notification_card_style.boxSX}>
-                    <Box sx={notification_card_style.iconBackgroundSx}>
-                        
-                    <NotificationIconSvg/>
-                        
-                    </Box>
-                <Box sx={{ cursor: 'pointer' }}
-                    onClick={() => redirectFnc(url, data)}
-                >
-                    <Box sx={notification_card_style.boxinSX}>
-                    <Typography
-                        variant="caption"
-                        color="text.disabled"
-                    >
-                        {notificationTime}
-
-                    </Typography>
-                    <Box sx={{opacity:"0","&:hover":{opacity:'1'}}}>
-                        <DeleteIcon/>
-                    </Box>
-                    </Box>
-                    <Typography
-                        variant="body2"
-                        style={{ fontWeight: is_read === false ? '600' : '100' }}
-                    >
+        <CardStyle {...rest}>
+            <StyleBox>
+                <NotificationImg>
+                    <NotificationIconSvg />
+                </NotificationImg>
+                <Box>
+                    <ContentStyle>
+                        <Typography variant="caption" color="text.disabled">
+                            {notificationTime}
+                        </Typography>
+                        <Box sx={{ display: "none", "&.MuiBox-root:hover": { display: 'block' } }}>
+                            <DeleteIcon />
+                        </Box>
+                    </ContentStyle>
+                    <Typography variant="body2">
                         {prescription}
                     </Typography>
-
                 </Box>
-            </Box>
-        </Box>
+            </StyleBox>
+        </CardStyle>
     );
 }
 
@@ -69,3 +42,8 @@ NotificationCard.propTypes = {
     prescription: PropTypes.string,
     notificationTime: PropTypes.string,
 };
+
+NotificationCard.defaultProps = {
+    prescription: "New feedback from Srikrishna (Chief of Engineering)",
+    notificationTime: "16 Feb 2023, 09:45 AM",
+  };
--- src/components/noticications/style.js
+++ src/components/noticications/style.js
@@ -1,29 +1,35 @@
-export const notification_card_style = {
-    rootSx: {
-        py: 2.1,
-        px: 2.5,
-        m: 1.2,
-        borderRadius: '4px',
-    },
-    iconBackgroundSx: {
-        background: "#EFEEFB 0% 0% no-repeat padding-box",
-        borderRadius: "4px",
-        opacity: 1,
-        padding:"4px 4px 0 4px"
-    },
-    boxSX: {
-        display: "flex",
-        flexWrap: "nowrap",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-        gap: "8px"
-    },
-    boxinSX: {
-        display: "flex",
-        justifyContent: "flex-start",
-        justifyContent: "space-between",
-        alignItems: "center",
-        flexWrap: "nowrap"
+import { Box, styled } from '@mui/material';
 
-    }
-}
+
+export const CardStyle = styled(Box)(() => ({
+    py: 2.1,
+    px: 2.5,
+    m: 1.2,
+    borderRadius: '4px',
+    
+}))
+
+
+export const StyleBox = styled(Box)(() => ({
+    display: "flex",
+    flexWrap: "nowrap",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    gap: "8px"
+}))
+
+export const NotificationImg = styled(Box)(() => ({
+    background: "#EFEEFB 0% 0% no-repeat padding-box",
+    borderRadius: "4px",
+    opacity: 1,
+    padding:"4px 4px 0 4px"
+}))
+
+export const ContentStyle = styled(Box)(() => ({
+    display: "flex",
+    justifyContent: "flex-start",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexWrap: "nowrap"
+
+}))
